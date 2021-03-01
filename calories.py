e28aab141fc0faffe6a3f90452bfe01de6fa1afc
Adi0704 <78225891+Adi0704@users.noreply.github.com>
2021-03-01 22:00:37 +0530

    Add files via upload
    Calories function....To calculate the amount of calories burnt

---
+++ calories.py
@@ -0,0 +1,15 @@
+def calorie1():
+inp=1
+calorie={1:557,2:10,3:50,4:590,5:604}
+while inp==1:
+     print("1.Cardio\n 2.Rest\n 3.Sleep\n4.Weight Training\n5.Cycling")
+     ch = int(input("Enter your choice:\n"))
+     if ch in calorie:
+         print("Average calories burned during",end="")
+         print(calorie[ch])
+     else:
+         print("Wrong input")
+     inp=int(input("Do you want to continue?\n 1.Yes 2.No"))
+
+
+
