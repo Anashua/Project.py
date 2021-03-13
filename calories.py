547ac1bfade919031d5e73f64636ac7cbd62410c
Adi0704 <78225891+Adi0704@users.noreply.github.com>
2021-03-14 01:28:39 +0530

    Add files via upload
    Calories gui

--- calories.py
+++ calories.py
@@ -1,15 +1,41 @@
-def calorie1():
-    inp=1
-    calorie={1:557,2:10,3:50,4:590,5:604}
-    while inp==1:
-        print("1.Cardio\n2.Rest\n3.Sleep\n4.Weight Training\n5.Cycling\n6.To exit function")
-        ch = int(input("Enter your choice:\n"))
-        if ch in calorie:
-            print("Average calories burned during",end="")
-            print(calorie[ch])
-        else:
-            print("Wrong input")
-            inp=input("Do you want to continue?\n 1.Yes 2.No   ")
-            if inp=="No" or "no" or "N" or "n" or 2:
-                return
-calorie1()
+from tkinter import *
+def func():
+    root=Tk()
+    root.title("Calorie")
+    root.geometry("500x500")
+    root.configure(bg='red')
+    def cardio():
+        message = "Calories burnt is 557 per hour"  # GET RETURNS THE CURRENT STRING THE USER ENTERS
+        label2 = Label(root, text=message)
+        label2.grid(row=2,column=1)
+    def rest():
+        message = "Calories burnt is 10 per hour"  # GET RETURNS THE CURRENT STRING THE USER ENTERS
+        label2 = Label(root, text=message)
+        label2.grid(row=3,column=1)
+    def sleep():
+        message = "Calories burnt is 50 per hour"  # GET RETURNS THE CURRENT STRING THE USER ENTERS
+        label2 = Label(root, text=message)
+        label2.grid(row=4,column=1)
+    def wt():
+        message = "Calories burnt is 590 per hour"  # GET RETURNS THE CURRENT STRING THE USER ENTERS
+        label2 = Label(root, text=message)
+        label2.grid(row=5,column=1)
+    def cycle():
+        message = "Calories burnt is 604 per hour"  # GET RETURNS THE CURRENT STRING THE USER ENTERS
+        label2 = Label(root, text=message)
+        label2.grid(row=6,column=1)
+    mylabel=Label(root,text="Welcome to Calorie predictor",bg="red",fg="white")
+    mylabel.grid(row=0,column=1000)
+    button1=Button(root,text="Cardio",command=cardio,bg="blue",fg="white")
+    button1.grid(row=2,column=0)
+    button2=Button(root,text="Rest",command=rest,bg="blue",fg="white")
+    button2.grid(row=3,column=0)
+    button3=Button(root,text="Sleep",command=sleep,bg="blue",fg="white")
+    button3.grid(row=4,column=0)
+    button4=Button(root,text="Weight Training",command=wt,bg="blue",fg="white")
+    button4.grid(row=5,column=0)
+    button5=Button(root,text="Cycling",command=cycle,bg="blue",fg="white")
+    button5.grid(row=6,column=0)
+    root.mainloop()
+
+#
