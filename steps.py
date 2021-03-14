ee903f4531163e82e3e488e4f1c13479f4b199e1
Anashua Krittika Dastidar <anashuad@gmail.com>
2021-03-14 17:30:04 +0530

    changed height

--- steps.py
+++ steps.py
@@ -1,19 +1,25 @@
-from tkinter import *
+from tkinter import*
 from tkinter import messagebox
+
 def func_main():
     #GETTING THE DATA
-    with open("data.txt","r") as data:
+    with open(r"C:\Users\HP\Desktop\Project.py\data.txt","r") as data:
         lis=list()
         for line in data:
             lis.append((line).split("\n"))
-
-    height = lis[3][0]
-    gender=lis[1][0]
-
+    height = int(lis[3][0])/30.48
+    def stride():
+        stride_len=30
+        hei=int(height*10)
+        dict={range(50,52):25,range(52,54):26,range(54,56):27,range(56,58):28,range(58,60):29,range(60,62):30,range(62,64):32,range(64,66):34,range(66,80):35}
+        for i in dict:
+            if hei in i :
+                stride_len=dict[i]
+        return stride_len
     def onClick():
-        steps=dist.get()//(12*int(height)*0.413)
-        messagebox.showinfo("Steps walked are",steps)
-        exit_button=Button(root,text="EXIT",command=root.destroy).grid(row=3,column=0)
+        steps=dist.get()/stride()
+        val=Label(root,text="Steps walked are {}".format(steps)).grid(row=3,column=0)
+        exit_button=Button(root,text="EXIT",command=root.destroy).grid(row=4,column=0)
 
     root=Tk()
     root.geometry("600x300")
