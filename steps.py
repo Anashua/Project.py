92b8d453e607c10372e06d6ac30bd5ab32e68cd5
Anashua Krittika Dastidar <anashuad@gmail.com>
2021-03-14 14:50:39 +0530

    download this

--- steps.py
+++ steps.py
@@ -7,19 +7,14 @@ def func_main():
         for line in data:
             lis.append((line).split("\n"))
 
-    height = int(lis[3][0])
+    height = lis[3][0]
     gender=lis[1][0]
 
     def onClick():
-        steps=dist.get()//stride()
+        steps=dist.get()//(12*int(height)*0.413)
         messagebox.showinfo("Steps walked are",steps)
         exit_button=Button(root,text="EXIT",command=root.destroy).grid(row=3,column=0)
 
-    def stride():
-        if gender=="Female":
-            return 12*height*0.413
-        else:
-            return 12*height*0.415
     root=Tk()
     root.geometry("600x300")
     dist=IntVar()
