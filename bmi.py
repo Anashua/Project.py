d12f31534b9bad2e2bb99a6e7a40c7f0a8aa9a6c
Anashua Krittika Dastidar <anashuad@gmail.com>
2021-03-14 16:08:30 +0530

    BMI WORKS!!!!!

--- bmi.py
+++ bmi.py
@@ -4,28 +4,31 @@ def body_mass_index():
 
     def child(hei, wei):
         index = bmi(wei, hei)
-        index = index * 100
+        index = int(index * 10)
+        val="indeterminate"
         category = {range(0, 5): 'Underweight', range(5, 85): 'Healthy weight', range(85, 95): 'At risk of overweight',
                     range(95, 7000): 'overweight'}
         for i in category.keys():
             if index in i:
-                return category[i], index
-
+                val= category[i]
+        return  index,val
     def adult(heigh, weigh):
         index = bmi(weigh, heigh)
-        index = index * 100
+        index = int(index * 10)
+        val="indeterminate"
         category = {range(0, 16): 'Severe Thinness', range(16, 17): 'Moderate thinness',
                     range(17, 19): 'Mild Thinness',
                     range(19, 25): 'normal', range(25, 30): 'overweight', range(30, 35): 'Obese Class 1 ',
                     range(35, 40): 'Obese Class 2', range(40, 1000): "Obese Class 3"}
         for i in category.keys():
             if index in i:
-                return category[i], index
+                val= category[i]
+        return index,val
 
     with open("data.txt","r") as data:
         lis=list()
         for line in data:
-           lis.append((line).split("\n"))
+            lis.append((line).split("\n"))
     age =int(lis[2][0])
     weight =int(lis[4][0])
     height = int(lis[3][0])/30.48
