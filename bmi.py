78f156f310feaae6503600d7a71517c414ecdb13
Anashua Krittika Dastidar <anashuad@gmail.com>
2021-03-14 13:31:40 +0530

    step wrong

--- bmi.py
+++ bmi.py
@@ -26,16 +26,14 @@ def body_mass_index():
         lis=list()
         for line in data:
            lis.append((line).split("\n"))
-    name=lis[0][0]
     age =int(lis[2][0])
     weight =int(lis[4][0])
     height = int(lis[3][0])
-    gender=lis[1][0]
-    print(name)
+
+
 
     if age in range(1, 18):
         bmi = child(height, weight)
     else:
         bmi = adult(height, weight)
     return bmi
-body_mass_index()
--- steps.py
+++ steps.py
@@ -1,26 +1,30 @@
-def steps():
-    state = input("True if walking , False if not ").capitalize()
-    move = False
-    if state == 'True' or 'T' or 'Yes' or 'Y' or 'TRUE' or 'YES':
-        move = True
-    else:
-        move = False
-    count = 0
-    res = 0
-    move = True
-    while move:
-        count += 1
-        res += 1
-        state = input("True if walking , False if not ").capitalize()
-        if state == 'False' or 'F' or 'No' or 'N' or 'FALSE' or 'NO':
-            break
-        else:
-            move=True
-        if res == 10:
-            res = 0
-            print(count * 50, " STEPS TAKEN !!!")
-            move=True
-    print("Steps take are ", count * 50)
+from tkinter import *
+from tkinter import messagebox
+def func_main():
+    #GETTING THE DATA
+    with open("data.txt","r") as data:
+        lis=list()
+        for line in data:
+            lis.append((line).split("\n"))
 
-    return count * 50
-steps()
+    height = int(lis[3][0])
+    gender=lis[1][0]
+
+    def onClick():
+        steps=dist.get()//stride()
+        messagebox.showinfo("Steps walked are",steps)
+        exit_button=Button(root,text="EXIT",command=root.destroy).grid(row=3,column=0)
+
+    def stride():
+        if gender=="Female":
+            return 12*height*0.413
+        else:
+            return 12*height*0.415
+    root=Tk()
+    root.geometry("600x300")
+    dist=IntVar()
+    entry=Label(root,text="Enter distance walked in m").grid(row=0,column=0)
+    entry_label=Entry(root,text="Enter distance walked in m",textvariable=dist).grid(row=1,column=0)
+    entry_button=Button(root,text="Click here",command=onClick).grid(row=2,column=0)
+    root.mainloop()
+func_main()
