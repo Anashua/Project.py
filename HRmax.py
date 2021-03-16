634a025e22d0e88063bc1b022e83223b78a10c4c
Adi0704 <78225891+Adi0704@users.noreply.github.com>
2021-03-16 23:26:32 +0530

    Add files via upload
    Final HRmax

--- HRmax.py
+++ HRmax.py
@@ -1,47 +1,77 @@
-def hrmax():
-    print("Welcome to HRmax calculator")
-    print("1.Male\n2.Female")
-    cho=int(input("Enter your option:"))
-    age=int(input("Enter your age:"))
-    hgh=int(input("Enter your height(cm):"))
-    wgt=int(input("Enter your weight(kgs):"))
-    def male(age,hgh,wgt):
-        Hrmax=220-age
-        upperi= Hrmax-10
-        loweri= Hrmax-30
-        upperar= Hrmax-50
-        lowerar=Hrmax-70
-        ##""c=bmi(age,hgh,wgt)""
-        print("Your HRmax is calculated to be ",Hrmax)
-        ##print("Your BMI is calculated to be ",c)
-        print("Your target heart rates for interval training are:\nDuring intervals :")
-        print(upperi,"-",loweri)
-        print("During active rest:\n")
-        print(upperar,"-",lowerar)
+from tkinter import *
+def func():
+    root = Tk()
+    root.title("Hrmax")
+    root.geometry("500x500")
+    root['background'] = '#856ff8'
 
 
-    def female(age, hgh, wgt):
-        Hrmax = 220 - age
-        upperi = Hrmax - 10
-        loweri = Hrmax - 30
-        upperar = Hrmax - 50
-        lowerar = Hrmax - 70
-        ##""c=bmi(age,hgh,wgt)""
-        print("Your HRmax is calculated to be ", Hrmax)
-        ##print("Your BMI is calculated to be ",c)
-        print("Your target heart rates for interval training are:\nDuring intervals :")
-        print(upperi, "-", loweri)
-        print("During active rest:")
-        print(upperar, "-", lowerar)
+    label1 = Label(root, text="Welcome to HRmax calculator", bg="green")
+    label1.grid(row=1, column=5)
+    label2 = Label(root, text="Name:", bg="red").grid(row=3, column=2)
+    entry1 = Entry(root, width=20)
+    entry1.grid(row=3,column=3)
+    label3 = Label(root, text="Age:", bg="red").grid(row=4, column=2)
+    e=Entry(root, width=20)
+    e.grid(row=4,column=3)
+    def myclick1():
+        message1="Your Hrmax is="
+        message=220-int(e.get())
+        lb4 = Label(root, text=message,bg="purple",fg="white")
+        lb4.grid(row=11,column=3)
+        lb3=Label(root,text=message1,bg="orange")
+        lb3.grid(row=11,column=1)
+    def myclick2():
+        message=220-int(e.get())-10
+        lb3=Label(root,text="Max heart rate=",bg="orange")
+        lb3.grid(row=12,column=1)
+        lb4=Label(root,text=message,bg="purple",fg="white")
+        lb4.grid(row=12,column=3)
+        message1=220-int(e.get())-30
+        lb1=Label(root,text="Min heart rate=",bg="orange")
+        lb1.grid(row=13,column=1)
+        lb2=Label(root,text=message1,bg="purple",fg="white")
+        lb2.grid(row=13,column=3)
+    button4= Button(root,text="Click for HRmax Results",command=myclick1,bg="pink").grid(row=5,column=3)
+    button5=Button(root,text="Click for Target heart rates during intervals",command=myclick2,bg="pink").grid(row=6,column=3)
+
+    root.mainloop()
+func()
+"""label1=Label(root,text="Welcome to HRmax calculator",bg="green")
+    label1.grid(row=1, column=2)
+    label2=Label(root,text="Name:",bg="red").grid(row=3,column=2)
+    entry1=Entry(root,width=20).grid(row=3,column=3)
+    label3=Label(root,text="Age:",bg="red").grid(row=4,column=2)
+    entry2 = Entry(root, width=20).grid(row=3, column=3)"""
 
 
-    if cho==1:
-        male(age,hgh,wgt)
-    elif cho==2:
-        female(age,hgh,wgt)
-    else:
-        print("Entered wrong option")
-hrmax()
+
+
+
+
+'''from tkinter import *
+#creating buttons
+root = Tk() #creates a window
+
+lb1 = Label(root, text = "Form")
+lb1.pack()
+#text box
+e = Entry(root, width = 50)
+e.pack()
+e.insert(0, "Enter your name") #Default text to be displayed as a prompt
+def myClick():
+     message = "hello "+e.get()
+     lb4 = Label(root, text = message)
+     lb4.pack()
+
+bt1 = Button(root, text = "Click Me!", command = myClick,bg = "blue",fg = "white")#callback
+bt1.pack()
+
+root.mainloop()'''
+
+
+
+
 
 
 
